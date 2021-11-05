bf817fc3bf309e354fc2049c60c5f4d2e064eba1
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-05 14:24:55 -0500

    Página para opciones de inicio

---
+++ frontend/src/pages/Main.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Link} from "react-router-dom";
+export default function Main(){
+    return (<main>
+        <div className="row d-flex justify-content-center row-cols-1 row-cols-md-3 mb-3 text-center">
+        
+        <div className="col">
+          <div className="card mb-4 rounded-3 shadow-sm">
+            <div className="card-header py-3">
+              <h4 className="my-0 fw-normal">Listado Productos</h4>
+            </div>
+            <div className="card-body">
+              <h3 className="card-title pricing-card-title"><small className="text-muted fw-light">Visita nuestro listado de productos</small></h3>
+              <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fillRule="currentColor" className="bi bi-list-check" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+            </svg>
+              <p><Link to="/products">Ver listado</Link></p>
+            </div>
+          </div>
+        </div>
+        <div className="col">
+          <div className="card mb-4 rounded-3 shadow-sm">
+            <div className="card-header py-3 ">
+              <h4 className="my-0 fw-normal">Catálogo de productos</h4>
+            </div>
+            <div className="card-body">
+            <h3 className="card-title pricing-card-title"><small className="text-muted fw-light">Conoce nuestro Catálogo</small></h3>
+              <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fillRule="currentColor" className="bi bi-grid-3x3-gap" viewBox="0 0 16 16">
+                <path d="M4 2v2H2V2h2zm1 12v-2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm5 10v-2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V7a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zm0-5V2a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1zM9 2v2H7V2h2zm5 0v2h-2V2h2zM4 7v2H2V7h2zm5 0v2H7V7h2zm5 0h-2v2h2V7zM4 12v2H2v-2h2zm5 0v2H7v-2h2zm5 0v2h-2v-2h2zM12 1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zm-1 6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-2a1 1 0 0 1-1-1V7zm1 4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1h-2z"/>
+              </svg>
+              <ul className="list-unstyled mt-1 mb-1">
+                <li><Link to="/catalogs">Consulta por menor precio</Link></li>
+                <li><a href="/#">Consulta por categoría</a></li>
+                <li><a href="/#">Consulta por nombre</a></li>
+              </ul>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+      </main>);
+}
