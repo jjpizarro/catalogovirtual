237a82124ec17f5ad21b69d5bb7387d9e2c3ac83
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-05 14:23:44 -0500

    página para el listado de los productos y gestionar el crud

---
+++ frontend/src/pages/ProductList.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductListTable from '../components/ProductListTable';
+import BackButton from "../components/BackButton";
+export default function ProductList(){
+   
+    return (<>
+        <BackButton />
+        <ProductListTable/>
+        </>)
+}
