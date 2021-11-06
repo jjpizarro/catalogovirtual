6b7dc5a6b698f47d3346c6517cc0b990bc011dc8
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-06 15:20:28 -0500

    se ajustan las rutas para todas la páginas

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -1,13 +1,17 @@
 import {Routes,Route} from "react-router-dom";
 import Main from "./pages/Main";
-import ProductCatalog from "./pages/ProductCatalog";
+import ProductCatalogPrice from "./pages/ProductCatalogPrice";
 import ProductList from "./pages/ProductList";
+import ProductCatalogCategory from "./pages/ProductCatalogCategory";
+import ProductCatalogName from "./pages/ProductCatalogName";
 export default function MyRoutes(){
     return (
     <Routes>
         <Route path="/" element={<Main/>} />
         <Route path="/products" element={<ProductList/>} />   
-        <Route path="/catalogs" element={<ProductCatalog/>} />  
+        <Route path="/catalog/price" element={<ProductCatalogPrice/>} />
+        <Route path="/catalog/category" element={<ProductCatalogCategory/>} />  
+        <Route path="/catalog/name" element={<ProductCatalogName/>} /> 
         
     </Routes>)
 }
