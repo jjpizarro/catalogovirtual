2de2a7b95806e8d2ad60c9c3e1ee07ef34831471
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-06 15:23:33 -0500

    se agrega de manera dinámica los datos de la tabla

--- frontend/src/components/ResultTable.jsx
+++ frontend/src/components/ResultTable.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Table from 'react-bootstrap/Table';
 
-const ResultTable = (props) =>{
+const ResultTable = ({products}) =>{
 
     return (<Table bordered hover>
         <thead>
@@ -15,14 +15,23 @@ const ResultTable = (props) =>{
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-          </tr>
+          {products.length > 0 ? products.map((product, index) =>(
+            <tr key={index}>
+              <td>{product.nombre}</td>
+              <td>{product.categoria}</td>
+              <td>{product.disponibilidad? 'Si':'No'}</td>
+              <td className="text-center">{product.precio}</td>
+              <td>{product.cantidad}</td>
+              <td><div className="text-center">
+                <img src={product.imagen} width="50" height="50" className="rounded" alt={product.descripcion} />
+                </div>
+              </td>
+            </tr>
+          )):(<tr>
+            <td colSpan={6}>No se encontraron registros! Realice una nueva búsqueda</td>
+          </tr>)
+
+          }
           
         </tbody>
       </Table>);
