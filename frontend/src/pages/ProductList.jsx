f763c934fec0a8974fbbd4d2209879a3ff1dd1da
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-06 11:08:36 -0500

    se agrega de manera dinámica el listado de productos

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import ProductListTable from '../components/ProductListTable';
 import BackButton from "../components/BackButton";
 import ModalForm from "../components/ModalForm";
+import ProductService from "../services/ProductService";
 
 
 export default function ProductList(){
+    const initialProductsState = [];
+    const [products, setProducts] = useState(initialProductsState);
+    
+    useEffect(()=>{
+        const getAllProducts = ()=>{
+            ProductService.getAll()
+                .then((response)=>{
+                    setProducts(response.data);
+                    console.log(response.data);
+                }).catch((e) => {
+                    console.log(e);
+                });
+        }
+        getAllProducts();
+    }, []);
+    console.log(products)
     const addProduct = (product)=>{
         console.log('Agregar producto');
         console.log(product);
     }
-    return (<>
-        <BackButton />
-        <ModalForm label="Agregar" styles="d-flex justify-content-end mb-2" variant="outline-primary"onCreateOrUpdate={addProduct} />
-        <ProductListTable/>
-        </>)
+    return (
+        <>
+            <BackButton />
+            <ModalForm label="Agregar" styles="d-flex justify-content-end mb-2" variant="outline-primary"onCreateOrUpdate={addProduct} />
+            <ProductListTable products ={products} />
+        </>
+        )
 }
