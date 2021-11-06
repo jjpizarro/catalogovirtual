193d75f5c4ffc5a89ae86fa0d9026e86e3b9715e
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-06 08:15:38 -0500

    se agrega un nuevo componente para manejar la búsqueda

--- frontend/src/pages/ProductCatalog.jsx
+++ frontend/src/pages/ProductCatalog.jsx
@@ -1,36 +1,19 @@
 import React from "react";
 
 import ResultTable from "../components/ResultTable";
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
 import BackButton from "../components/BackButton";
+import SearchForm from "../components/SearchForm";
 export default function ProductCatalog(){
-    
+    const updateTable = (products)=>{
+
+    }
     
     return (<>
                 
                 <BackButton/>
-            
-                <Form className="d-flex justify-content-end">
-                    <Row className="align-items-center g-3">
-                        <Col xs="auto">
-                            <Form.Label htmlFor="precio" className="col-form-label">
-                                <span className="fw-bold">Consultar por menor precio</span>
-                            </Form.Label>
-                            
-                        </Col>
-                        <Col xs="auto">
-                            <Form.Control type="number" className="mb-2" id="precio" />
-                        </Col>
-                        <Col xs="auto">
-                            <Button type="submit" className="mb-2">
-                                    Buscar
-                            </Button>
-                        </Col>
-                    </Row>
-                 </Form>
+                <SearchForm updateTable={updateTable}/>
+                
                   
                 <ResultTable/>
             </>);
