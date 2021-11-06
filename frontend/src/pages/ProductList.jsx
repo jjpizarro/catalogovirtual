0b6d76a2e553c47620d1f539ac7fea70f6fe0ba6
julianpizarro <julian.pizarro.mt@correo.usa.edu.co>
2021-11-06 08:14:24 -0500

    se agrega el modal para add

--- frontend/src/pages/ProductList.jsx
+++ frontend/src/pages/ProductList.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ProductListTable from '../components/ProductListTable';
 import BackButton from "../components/BackButton";
+import ModalForm from "../components/ModalForm";
+
+
 export default function ProductList(){
-   
+    const addProduct = (product)=>{
+        console.log('Agregar producto');
+        console.log(product);
+    }
     return (<>
         <BackButton />
+        <ModalForm label="Agregar" styles="d-flex justify-content-end mb-2" variant="outline-primary"onCreateOrUpdate={addProduct} />
         <ProductListTable/>
         </>)
 }
